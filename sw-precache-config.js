dc906352a151b6c5d26bfe3a93be7a806a33f8fe
uncompiled <chris@uncompiled.org>
2017-02-07 21:13:08 -0500

    Update service worker precache configuration

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,6 +5,8 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/**/*',
+    '/src/**/*'
   ],
   navigateFallback: 'index.html',
 };
