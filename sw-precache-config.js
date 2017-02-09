627653934c6c49793e6787e003ffec75e37c398d
uncompiled <chris@uncompiled.org>
2017-02-08 21:35:21 -0500

    Cache more things :|

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -4,7 +4,7 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/**/*',
     '/images/**/*',
     '/src/**/*'
   ],
